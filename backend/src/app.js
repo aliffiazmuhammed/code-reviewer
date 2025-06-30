5d5060a34d39a03c33de6671e226eafa3c4d0d15
alif-fiaz-muhammed <alif.m@ylogx.io>
2025-06-30 21:50:55 +0530

    project finished

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,11 @@ const airoutes = require('../router/ai.router')
 const app = express();
 const cors = require('cors')
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: '*',  // Allow local frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 app.get('/',(req,res)=>{
     res.send("hello world")
